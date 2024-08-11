5c15c26cb969fb7264dfd11d85f871c44a56ddb4
Li Xiang <1712818580@qq.com>
2024-08-11 15:37:42 +0800

    commit 1
    1

---
+++ index.js
@@ -0,0 +1,7 @@
+function showContactInfo() {
+    document.getElementById('contact-info').style.display = 'block';
+  }
+
+  function showImage() {
+    document.getElementById('image').style.display = 'block';
+  }
